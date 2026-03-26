edaf938a935480c77653e7d0059aad1e1fb68976
severrir <sabakurashvili903@gmail.com>
2026-03-26 23:40:25 +0400

    Add comment about module placement in Fx.lua
    Add comment indicating module placement for SkillReplicator.

---
+++ AdvancedCombatSystem/StarterCharacterScripts/Fx.lua
@@ -0,0 +1,22 @@
+-- module this should be in SkillReplicator but i cant do that in github
+local BaseEffects = {}
+
+local function fetchEffect(FXName)
+	return BaseEffects[FXName]
+end
+
+local function load(Module)
+	local Effect = require(Module)
+	for i, v in pairs(Effect) do
+		BaseEffects[i] = v
+	end
+	Effect.getfx = fetchEffect()
+end
+
+for i, v in pairs(script:GetChildren()) do
+	if v:IsA("ModuleScript") then
+		load(v)
+	end
+end
+
+return BaseEffects
